1782b3e54fa309a7d6cdba43fddabaf8682ae774
ultimacrown <68177832+ultimacrown@users.noreply.github.com>
2021-12-18 16:23:27 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,20 +1,77 @@
-input.onButtonPressed(Button.A, function () {
+input.onPinPressed(TouchPin.P0, function () {
     basic.showLeds(`
         . . . . .
-        # # . . .
-        # # . . .
-        # # . . .
+        . . . . .
+        # # # # #
+        . . . . .
         . . . . .
         `)
-    serial.writeLine("A")
+})
+input.onGesture(Gesture.TiltRight, function () {
+    if (mode == 1) {
+        serial.writeString("R")
+        basic.showArrow(ArrowNames.East)
+    }
+})
+input.onButtonPressed(Button.A, function () {
+    if (mode == 1) {
+        basic.showLeds(`
+            . . . . .
+            # # . . .
+            # # . . .
+            # # . . .
+            . . . . .
+            `)
+        serial.writeLine("A")
+    }
+})
+input.onGesture(Gesture.TiltLeft, function () {
+    if (mode == 1) {
+        serial.writeString("L")
+        basic.showArrow(ArrowNames.West)
+    }
+})
+input.onGesture(Gesture.LogoUp, function () {
+    if (mode == 1) {
+        serial.writeString("D")
+        basic.showArrow(ArrowNames.South)
+    }
+})
+input.onGesture(Gesture.LogoDown, function () {
+    if (mode == 1) {
+        serial.writeString("U")
+        basic.showArrow(ArrowNames.North)
+    }
 })
 input.onButtonPressed(Button.B, function () {
+    if (mode == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . # #
+            . . . # #
+            . . . # #
+            . . . . .
+            `)
+        serial.writeLine("B")
+    }
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
     basic.showLeds(`
-        . . . . .
-        . . . # #
-        . . . # #
-        . . . # #
-        . . . . .
+        . . # . .
+        . . # . .
+        . . # . .
+        . . # . .
+        . . # . .
         `)
-    serial.writeLine("B")
+})
+let mode = 0
+mode = 0
+basic.forever(function () {
+    if (mode == 0) {
+    	
+    } else if (false) {
+    	
+    } else {
+    	
+    }
 })
