115760ace3546e121014037345f99d4ce2116dd3
ultimacrown <68177832+ultimacrown@users.noreply.github.com>
2021-12-21 04:04:13 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,70 +1,71 @@
 function menu () {
+    basic.showIcon(IconNames.Happy)
     if (input.buttonIsPressed(Button.A)) {
         mode = 1
+        basic.clearScreen()
+        basic.pause(1000)
     } else if (input.buttonIsPressed(Button.B)) {
         mode = 2
+        basic.clearScreen()
+        basic.pause(1000)
     }
-    basic.showIcon(IconNames.Happy)
 }
 function serial2 () {
-    if (input.isGesture(Gesture.TiltLeft)) {
-        serial.writeLine("L")
-        basic.showArrow(ArrowNames.West)
-    } else if (input.isGesture(Gesture.TiltRight)) {
-        serial.writeLine("R")
-        basic.showArrow(ArrowNames.East)
-    } else if (input.isGesture(Gesture.LogoUp)) {
-        serial.writeLine("D")
-        basic.showArrow(ArrowNames.South)
-    } else if (input.isGesture(Gesture.LogoDown)) {
-        serial.writeLine("U")
-        basic.showArrow(ArrowNames.North)
-    } else if (input.buttonIsPressed(Button.A)) {
-        serial.writeLine("A")
-        basic.showLeds(`
-            . . . . .
-            # # . . .
-            # # . . .
-            # # . . .
-            . . . . .
-            `)
+    if (input.buttonIsPressed(Button.AB)) {
+        serial.writeLine("H")
     } else if (input.buttonIsPressed(Button.B)) {
         serial.writeLine("B")
-        basic.showLeds(`
-            . . . . .
-            . . . # #
-            . . . # #
-            . . . # #
-            . . . . .
-            `)
-    } else if (input.buttonIsPressed(Button.AB)) {
-        serial.writeLine("H")
-        basic.showLeds(`
-            . . . . .
-            # # . # #
-            # # . # #
-            # # . # #
-            . . . . .
-            `)
-    } else if (input.pinIsPressed(TouchPin.P2)) {
+    } else if (input.buttonIsPressed(Button.A)) {
+        serial.writeLine("A")
+    } else if (input.rotation(Rotation.Pitch) < -30) {
+        serial.writeLine("U")
+    } else if (input.rotation(Rotation.Roll) < -45) {
+        serial.writeLine("L")
+    } else if (input.rotation(Rotation.Roll) > 45) {
+        serial.writeLine("R")
+    } else if (input.rotation(Rotation.Pitch) > 30) {
+        serial.writeLine("D")
+    } else {
+    	
+    }
+    if (input.pinIsPressed(TouchPin.P2)) {
         serial.writeLine("E")
         mode = 0
+    } else if (input.pinIsPressed(TouchPin.P1)) {
+        serial.writeLine("S")
     } else {
     	
     }
 }
-function doSomething () {
-    mode = 0
-    basic.showIcon(IconNames.Sad)
+input.onGesture(Gesture.Shake, function () {
+	
+})
+function beep () {
+    if (input.buttonIsPressed(Button.A) || input.buttonIsPressed(Button.B)) {
+        brightness = input.lightLevel()
+        brightness += 10
+        music.stopAllSounds()
+    } else if (brightness < input.lightLevel()) {
+        music.ringTone(262)
+    } else {
+        music.stopAllSounds()
+    }
+    if (input.pinIsPressed(TouchPin.P2)) {
+        mode = 0
+        music.stopAllSounds()
+    }
 }
+let brightness = 0
 let mode = 0
 mode = 0
+music.setVolume(255)
+music.stopAllSounds()
 basic.forever(function () {
     if (mode == 0) {
         menu()
     } else if (mode == 1) {
         serial2()
     } else {
-        doSomething()
+        beep()
     }
 })
